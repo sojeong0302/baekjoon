e1fdb9d227f054f12fd1b85426ce5a2dc556e345
sojeong0302 <01duqrl@naver.com>
2023-03-01 13:26:13 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int A;
+        int B;
+        Scanner sc = new Scanner(System.in);
+        A=sc.nextInt();
+        B=sc.nextInt();
+        System.out.println(A+B);
     }
 }
